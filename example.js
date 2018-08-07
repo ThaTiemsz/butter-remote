a1ae9497eff09e300bec1ffd811be16c520783e8
Tiemen <Tiemen2@live.nl>
2018-08-07 06:05:21 +0200

    Update example

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ br.ping()
 br.getCurrentTab().then(result => console.log("Current tab:", result))
 
 async function list() {
-    const { list } = await br.getCurrentList("2")
+    const { list } = await br.getCurrentList([2])
     return console.log(list[0].title)
 }
 list()
